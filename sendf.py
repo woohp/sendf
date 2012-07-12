414e5eb389b77c83924441df0f695ab0c0d71d56
Hui Peng Hu <woohp135@gmail.com>
2012-07-11 19:36:12 -0700

    first version.

---
+++ sendf.py
@@ -0,0 +1,179 @@
+import sys
+import signal
+import uuid
+import os
+import tarfile
+import random
+import argparse
+import tempfile
+import datetime
+import cherrypy
+from cherrypy.lib.static import serve_file
+from upnp import *
+
+
+def check_files_exist(filenames):
+    for filename in filenames:
+        if os.path.exists(filename) == False:
+            return False
+    return True
+
+def get_internal_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('google.com', 0))
+        return s.getsockname()[0]
+    except:
+        return None
+
+class SendF(object):
+    def __init__(self, filenames):
+        self.filenames = filenames
+        self.internal_ip = None
+        self.external_ip = None
+        self.port = None
+        self.uuid = str(uuid.uuid4())
+        self.upnp = None
+        self.port_forwarded = False
+        self.compressed = None
+
+        self.start_time = None
+        self.download_count = 0
+
+    def initialize(self):
+        # first, check that all files exists
+        if check_files_exist(self.filenames) == False:
+            return False
+        self.filepaths = [os.path.abspath(f) for f in self.filenames]
+
+        # try getting the internal IP
+        self.internal_ip = get_internal_ip()
+        if self.internal_ip == None:
+            return False
+
+        # first, do UPnP discovery
+        upnp = UPnPPlatformIndependent()
+        self.upnp = upnp
+        upnp.discover()
+        if upnp.found_wanted_services():
+            self.external_ip = upnp.get_ext_ip()
+            for i in xrange(10):
+                port = random.randint(1024, 8192)
+                try:
+                    upnp.add_port_map(self.internal_ip, port)
+                except:
+                    continue
+                self.port = port
+                self.port_forwarded = True
+                break
+
+        # if UPnP did not work, then use the internal IP as the external IP
+        if not self.port_forwarded:
+            self.external_ip = self.internal_ip
+
+        self.start_time = datetime.datetime.now()
+        return True
+
+    def finalize(self):
+        if self.port_forwarded:
+            self.upnp.del_port_map(self.port)
+            self.port_forwarded = False
+        if self.compressed:
+            os.remove(self.compressed)
+            self.compressed = None
+
+    def quit(self):
+        self.finalize()
+        cherrypy.engine.exit()
+
+    def _create_archive(self, archive_filename, compress='gz'):
+        if compress == 'gz':
+            f = tarfile.open(archive_filename, 'w:gz')
+        elif compress == 'bz2':
+            f = tarfile.open(archive_filename, 'w:bz2')
+        elif compress == 'tar' or compress == 'none':
+            f = tarfile.open(archive_filename, 'w')
+
+        for path in self.filepaths:
+            basename = os.path.basename(path)
+            f.add(path, basename)
+
+        f.close()
+
+    def default(self, uuid):
+        if uuid != uuid:
+            return
+        if check_files_exist(self.filepaths) == False:
+            self.quit()
+
+        if len(self.filepaths) > 1 or os.path.isdir(self.filepaths[0]):
+            file_to_send = os.path.join(tempfile.gettempdir(), self.uuid)
+            self._create_archive(file_to_send)
+            self.compressed = file_to_send
+            name = "archive.tgz"
+        else:
+            file_to_send = self.filepaths[0]
+            name = None
+
+        self.download_count += 1
+        return serve_file(
+                file_to_send,
+                content_type="application/x-download",
+                disposition="attachment",
+                name=name)
+
+    default.exposed = True
+
+    
+    def __repr__(self):
+        return "http://%s:%d/%s" % (self.external_ip, self.port, self.uuid)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="sendf")
+#    parser.add_argument("-c", "--count", type=int, default=2,
+#            help="maximum download count before the link expires")
+    parser.add_argument("-d", "--duration", type=int, default=30,
+            help="time before the link expires, in minutes")
+    parser.add_argument("-o", "--output", type=str, default='',
+            help="name of the file the user will receive it as")
+    parser.add_argument("files", type=str, nargs="+",
+            help="files to send")
+    args = vars(parser.parse_args())
+
+    # stop cherrypy from printing stuff to the screen
+    cherrypy.log.screen = False
+    cherrypy.checker.on = False
+
+    # initialize
+    sendf = SendF(args['files'])
+    sendf.initialize()
+    print sendf
+
+    # setup signal handlers
+    def signal_handler(signal, frame):
+        sendf.finalize()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGQUIT, signal_handler)
+
+    # set function to check whether the duration has passed
+    duration = datetime.timedelta(minutes=args['duration'])
+    check_frequency = args['duration'] * 10 # check every 10 seconds
+    def check_duration():
+        if datetime.datetime.now() - sendf.start_time >= duration:
+            sendf.quit()
+    cherrypy.process.plugins.Monitor(
+            cherrypy.engine,
+            check_duration,
+            check_frequency).subscribe()
+
+    # config server and start it up
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                            'server.socket_port': sendf.port})
+    cherrypy.quickstart(sendf)
+
+if __name__ == '__main__':
+    main()
+
